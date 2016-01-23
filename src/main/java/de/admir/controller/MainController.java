b0a1211d2b1e2d7123c577ee238c4dbe8b826744
shimshir <me.admir@gmail.com>
2016-01-23 03:22:41 +0100

    thymeleaf templating and resource reloading

--- src/main/java/de/admir/controller/MainController.java
+++ src/main/java/de/admir/controller/MainController.java
@@ -1,15 +1,11 @@
 package de.admir.controller;
 
-import de.admir.dto.AppointmentData;
 import de.admir.service.AppointmentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.*;
 
 /**
  * Author:  Admir Memic
@@ -17,17 +13,18 @@ import java.util.*;
  * E-Mail:  admir.memic@dmc.de
  */
 
-@RestController
+@Controller
 public class MainController {
 
 	@Autowired
 	private AppointmentService appointmentService;
 
 	@RequestMapping(path = "/", method = RequestMethod.GET)
-	public ResponseEntity<List<AppointmentData>> index(
-			@RequestParam(name="forceupdate", defaultValue = "false") boolean forceUpdate) {
+	public String getIndex(
+					@RequestParam(name = "forceupdate", defaultValue = "false") boolean forceUpdate, ModelMap model) {
 		if (forceUpdate)
 			appointmentService.updateFreeAppointmentsMap();
-		return ResponseEntity.ok(appointmentService.getAppointmentDatas());
+		model.addAttribute("appointmentDatas", appointmentService.getAppointmentDatas());
+		return "index";
 	}
 }
